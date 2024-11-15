25c9f3e7dffc335970a538ca126819ad72fd0f43
irish-mae <tagayloirishlabis@gmail.com>
2024-11-15 14:23:43 +0800

    added marked complete and delete

--- taskmanager.py
+++ taskmanager.py
@@ -15,3 +15,16 @@ IRISH
     mark as complete
     delete task
 """
+def mark_task_complete(self, index):
+        if 0 <= index < len(self.tasks):
+            self.tasks[index].mark_complete()
+            print(f"Task '{self.tasks[index].name}' marked as complete.")
+        else:
+            print("Invalid task number.")
+
+def delete_task(self, index):
+        if 0 <= index < len(self.tasks):
+            task_name = self.tasks.pop(index).name
+            print(f"Task '{task_name}' deleted.")
+        else:
+            print("Invalid task number.")
