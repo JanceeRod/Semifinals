1bba8aa23ee79b6f0eb675946e60dbe1aa4b033c
Jancee <jancee800@gmail.com>
2024-11-15 13:49:32 +0800

    initialized file and assign tasks

---
+++ taskmanager.py
@@ -0,0 +1,17 @@
+# JOHN RICO CAGALITAN
+# JANCEE ROD COLEGIO
+# IRISH MAE TAGAYLO
+
+"""
+JANCEE 
+    add task
+    view task
+
+RICO
+    filter task
+    edit task
+
+IRISH
+    mark as complete
+    delete task
+"""
